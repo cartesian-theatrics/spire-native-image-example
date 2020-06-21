0f4fa39ada44b3b5c7bab7aa42f6bfc013ce49b1
John Collins <john.collins@braincorp.com>
2020-06-21 16:40:53 -0700

    Working with core.async

--- src/core.clj
+++ src/core.clj
@@ -1,15 +1,34 @@
 (ns core
   (:require
-   [spire.core]
+   [spire.state]
    [spire.config :as config]
+   [spire.output.core :as output]
+   [spire.output.default]
+   [spire.output.events]
+   [spire.output.quiet]
+   [puget.printer :as puget]
+   [clojure.tools.cli :as cli]
    [spire.module.shell :refer [shell]]
-   [spire.transport :refer [ssh]])
+   [spire.transport :refer [ssh]]
+   [clojure.core.async :refer [go-loop <!! <! timeout]])
   (:gen-class))
 
+(def cli-options
+  [[nil "--help" "Print the command line help"]
+   [nil "--target TARGET" "SSH target. Ex. --target '{:username \"sarah\" :hostname \"sandbox\"}'"]])
+
 (defn -main [& args]
-  (config/init!)
-  (clojure.lang.RT/loadLibrary "spire")
-  (ssh "brain@sandbox" (shell {:cmd "echo 'hello'"}))
-  (flush)
-  (Thread/sleep 1000)
-  (println "Finished!"))
+  (let [{:keys [options summary]} (cli/parse-opts args cli-options)]
+    (cond (:help options) (println summary)
+          (:target options)
+          (do (config/init!)
+              (output/print-thread :default)
+              (clojure.lang.RT/loadLibrary "spire")
+              (<!! (go-loop []
+                     (puget/cprint (ssh (-> options :target clojure.edn/read-string)
+                                        (shell {:cmd "echo 'hello'"})))
+                     (<! (timeout 2000))
+                     (recur))))
+          :else
+          (do (println "Missing ssh target")
+              (println summary)))))
