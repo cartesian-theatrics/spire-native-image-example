6324c67092b06eb0639c4bfc96b8626ce85218d3
John Collins <john.collins@braincorp.com>
2020-06-21 15:14:50 -0700

    Mostly working

--- src/core.clj
+++ src/core.clj
@@ -7,6 +7,9 @@
   (:gen-class))
 
 (defn -main [& args]
-  (config/init!)  ;; when running under graal, extracts the shared library to users home directory and sets up the java library path. You will need to have the .so or .dynlib file bundled in your resources for this extraction to work.
-  (clojure.lang.RT/loadLibrary "spire")  ;; load the C library
-  (ssh "brain@sandbox" (shell {:cmd "echo 'hello'"})))
+  (config/init!)
+  (clojure.lang.RT/loadLibrary "spire")
+  (ssh "brain@sandbox" (shell {:cmd "echo 'hello'"}))
+  (flush)
+  (Thread/sleep 1000)
+  (println "Finished!"))
